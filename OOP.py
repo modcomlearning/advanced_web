9217bb26e2d1836fda29aa1f79a529fb3aa50068
Beth Miano <bethernest50@gmail.com>
2020-11-16 10:48:39 +0300

    8th Commit

---
+++ OOP.py
@@ -0,0 +1,11 @@
+
+# a bank object
+class Account:
+
+    def __init__(self):
+        self.name = 'Your name'
+        self.branch = 'Waiyaki way'
+        self.age = 10
+        self.accno = 1000050
+        self.balance = 50
+        
