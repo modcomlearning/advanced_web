f330c3910f51ddd1a7577b32ad9425e02392e2e2
Beth Miano <bethernest50@gmail.com>
2020-11-18 09:36:54 +0300

    9th Commit

--- OOP.py
+++ OOP.py
@@ -1,7 +1,6 @@
 
 # a bank object
 class Account:
-
     def __init__(self):
         self.name = 'Your Name'
         self.branch = 'Waiyaki way'
@@ -19,10 +18,41 @@ class Account:
         print('You deposited ', amount)
         print('Your New balance is ', self.balance)
         print('Thank you')
+     # withdrawal
+
+    def withdraw(self, amount):
+        print('Welcome ', self.name)
+        print('Your Current balance is ', self.balance)
+        if amount > self.balance:
+            print('You have insufficient Funds')
+            
 
 
-object = Account()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+object = Account()  # instantiate
 object.deposit(2000)
 object.deposit(2000)
+# Assignment
+# do a withdrawal function, if possible check that we can't withdraw more than balance
+#                  if possible check we can't withdraw a zero
+                   # Hint.. use if statements
+# do a checkbal function
+
 
 
